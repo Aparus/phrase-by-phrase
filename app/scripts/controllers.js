87006a5efaab9b0652ee15c0f3325fd8653bb7ec
Aparus <rustamapaev@gmail.com>
2017-01-29 15:21:58 +0300

    Addeded tabs , lines in text mode

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -7,7 +7,7 @@ angular.module('booktitresApp')
             $scope.page = {mediaLink:"http://ia801406.us.archive.org/13/items/alice_in_wonderland_librivox/wonderland_ch_01.mp3", mediaType:""} 
             
             $scope.isAudio = false
-            
+            $scope.tab = 1
             $scope.page.phrases = phrasesFactory.getPhrases()
            
            //phrasesFactory.setPhrases()
@@ -28,6 +28,33 @@ angular.module('booktitresApp')
             $scope.mediaFactory = mediaFactory
             $scope.phrasesFactory = phrasesFactory
             $scope.subsFactory = subsFactory
+            
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+            
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                console.log($scope.tab)
+            }
+            
+            $scope.fuck = function (){
+                console.log("bbbbb")
+            }
+
+            $scope.readSubs = function(subs_text){
+
+              var subs_type = subsFactory.type(subs_text)
+              
+              
+                if( (subs_type == "srt" ) || (subs_type == "webvtt" ) )
+                    phrasesFactory.setPhrases(subsFactory.parseWebvttSrt(subs_text));
+                else if(subs_type == "ass")
+                    phrasesFactory.setPhrases(subsFactory.parseAss(subs_text));
+                else if(subs_type = "htmlAudioBook")
+                        phrasesFactory.setPhrases(subsFactory.parseAudioBookHtml(subs_text));
+                        $scope.page.phrases = phrasesFactory.getPhrases()
+            }
             
 
             
--- app/scripts/services/subsFactory.js
+++ app/scripts/services/subsFactory.js
@@ -2,17 +2,23 @@
 
 angular.module('booktitresApp')
         .service('subsFactory', ['$resource', 'baseURL', 'phrasesFactory', function($resource, baseURL, phrasesFactory) {
-
+            
+            
               this.subs = []
             
             // recognition of subs format by text patterns 
               this.type = function(text){
-                if (/^WEBVTT/.test(text)) return "webvtt"
-                else if(/^\d\s+?\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/m.test(text)) return "srt"
-                else if(/Dialogue: 0,\d:\d\d:\d\d.\d\d,\d:\d\d:\d\d.\d\d,Default,,0,0,0,,/.test(text)) return "ass"
-                else if(/<!--audio start="(.+?)" end="(.+?)"-->([\s\S]+?)<!--\/audio-->/.test(text)) return "HtmlAudioBook"
-                else if(/talk-transcript__fragment/.test(text)) return "ted"
-                else return "unknown"
+            
+                var type = "unknown"
+                if (/^WEBVTT/.test(text)) type = "webvtt"
+                else if(/^\d\s+?\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/m.test(text)) type = "srt"
+                else if(/Dialogue: 0,\d:\d\d:\d\d.\d\d,\d:\d\d:\d\d.\d\d,Default,,0,0,0,,/.test(text)) type = "ass"
+                else if(/<!--audio start="(.+?)" end="(.+?)"-->([\s\S]+?)<!--\/audio-->/.test(text)) type = "htmlAudioBook"
+                else if(/talk-transcript__fragment/.test(text)) type = "ted"
+                
+                console.log(type)
+                return type
+                
               }
 
               this.push = function(phrase){
@@ -31,10 +37,10 @@ angular.module('booktitresApp')
 
                 for (var i=1; i < subs_array.length; i++) {
 
-                  line_array = subs_array[i].split("\n")
+                  var line_array = subs_array[i].split("\n")
                   //00:00:50.000 --> 00:00:52.780
 
-                    srt_timing_array = line_array[1].split(" --> ")
+                    var srt_timing_array = line_array[1].split(" --> ")
 
                         sub.timingStart = this.timeToSeconds(srt_timing_array[0])
                         sub.timingEnd = this.timeToSeconds(srt_timing_array[1])
@@ -54,7 +60,7 @@ angular.module('booktitresApp')
                 var subs_array = text.split('\n')
                 subs_array.unshift("ASS")
                 for (var i=1; i < subs_array.length; i++) {
-                  line_array = subs_array[i].split(",")
+                  var line_array = subs_array[i].split(",")
                   //Dialogue: 0,0:00:13.22,0:01:02.37,Default,,0,0,0,, There is text of phrase in media
                   sub.timingStart = this.timeToSeconds(line_array[1])
                   sub.timingEnd = this.timeToSeconds(line_array[2])
@@ -69,14 +75,19 @@ angular.module('booktitresApp')
 
             //this function parse code of AudioBook to array of objects with the structure [{timingStart,timingEnd,text}] ; separates all code into the phrases objects
              this.parseAudioBookHtml = function(text){
-                var phrases = []
+                var phrases = [{timingStart: 0, timingEnd: 0, text: ""}]
                 var template0 = /<!--audio start="(.+?)" end="(.+?)"-->([\s\S]+?)<!--\/audio-->/g
                 var template = /<!--audio start="(.+?)" end="(.+?)"-->([\s\S]+?)<!--\/audio-->/
                 var phrases0 = text.match(template0); // array with whole phrases 
+                 var subs = this
+                console.log(phrases0)
                 phrases0.forEach(function(phrase0){
                     var phrase = phrase0.match(template)
-                    phrases.push({timingStart: phrase[1], timingEnd: phrase[2], text: phrase[3].replace(/\n/g, " ")})
+                    var t0 = subs.timeToSeconds(phrase[1])
+                    var t1 = subs.timeToSeconds(phrase[2])
+                    phrases.push({timingStart: t0, timingEnd: t1, text: phrase[3].replace(/\n/g, " ")})
                 })
+                console.log(phrases)
                 return phrases
 
             }
@@ -84,7 +95,7 @@ angular.module('booktitresApp')
               //00:00:06.960 --> 6.96
               this.timeToSeconds = function(time){
                 var time_array = time.replace(/,/g,'.').split(":")
-                return (parseFloat(time_array[2]) + parseFloat(time_array[1]*60) + parseFloat(time_array[0]*3600)).toFixed(2)
+                return parseFloat((parseFloat(time_array[2]) + parseFloat(time_array[1]*60) + parseFloat(time_array[0]*3600)).toFixed(1))
               }
 
               this.languageDetector = function(text) {
